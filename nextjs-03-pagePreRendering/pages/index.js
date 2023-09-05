516064316c443b1da4cd84973d60d4e1e543883f
Linstedim <imalenko@roko-practicum.com>
2023-09-05 21:15:45 +0300

    feat: add prerendering through getStaticProps func

---
+++ nextjs-03-pagePreRendering/pages/index.js
@@ -0,0 +1,24 @@
+import fs from "fs/promises";
+import path from "path";
+
+function HomePage({ products }) {
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: data,
+  };
+}
+export default HomePage;
